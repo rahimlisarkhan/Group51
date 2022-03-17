86a648b5fbe9369e40fde505b9b4163d4552ba87
rahimlisarkhan <rahimlisarkhan@gmail.com>
2022-03-17 21:57:52 +0400

    update

---
+++ Fundamentals/topic6/intro/index.js
@@ -0,0 +1,100 @@
+// Step1
+// var a = "John Doe job"
+// var userName = prompt("Adiniz nedir")
+// var tesdiqle = confirm("Pisikleri sevirsen?")
+
+// alert(tesdiqle)
+// console.log(a);
+
+//console.log("Hello World");
+
+//Step2
+// var integer = 5;
+// var integerFloat = 5.2;
+// var string = "John Doe";
+// var boolean = true; //false true
+// var undefiend = undefined;
+// var nullValue = null;
+
+// var a = 4;
+// var b = 4;
+
+// // var c = 5
+// // c = c + 15
+
+// var c = 5;
+
+// c +=11;
+
+// var a = "50"
+// var b = 5
+// var d = a - b
+
+// console.log(d);
+
+// var a = prompt("Reqem gorek yaddasimda tutdugumdurmu?")
+// var a = 5
+
+// var netice = a
+
+// netice = netice + 5
+
+// console.log(a);
+// console.log(netice);
+
+// var integer = 0;
+// var string = "";
+// var boolean = false; //false true
+// var undefiend = undefined;
+// var nullValue = null;
+
+// var a = 18
+
+// if(a === ""){
+//     console.log("deyer var");
+// }else{
+//     console.log("deyer yoxdur");
+// }
+
+// var age = prompt("Yasinizi yazin");
+
+// if (age >= 18 || age > 20) {
+//   console.log("icaze verildi");
+// } else if (age < 18) {
+//   console.log("daxil olmaq qadagandir");
+// }
+// else {
+//   console.log("cox yasiniz var");
+// }
+
+// if( age < 40){
+//     console.log("sizin ucun yaxsi kompaniyalar olacaq");
+// }
+
+// var starGame = confirm("Oyuna baslayaq?");
+
+// if (starGame) {
+//   var q1 = prompt("Goy uzunun rengi");
+
+//   if (q1 === "mavi") {
+//     alert("ela bildin");
+
+//     var q2 = prompt("2 + 2");
+
+//     if (q2 == 4) {
+//       alert("ela bildin");
+
+//       var q3 = prompt("4 * 12");
+
+//       if (q3 == 48) {
+//         alert("Tebrikler oyun bitdi");
+//       } else {
+//         alert("Bu sefer alinmadi");
+//       }
+//     } else {
+//       alert("yaxsi gelmisdin heyif");
+//     }
+//   } else {
+//     alert("Bu sefer alinmadi");
+//   }
+// }
