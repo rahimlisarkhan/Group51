b0e0c278f2d593654af1223eb3696272dfce6549
rahimlisarkhan <rahimlisarkhan@gmail.com>
2022-03-19 23:30:52 +0400

    update

---
+++ Fundamentals/topic6/array/index.js
@@ -0,0 +1,131 @@
+// console.log("Salam");
+// var userName = "John Doe";
+// var a = 5;
+
+// var b = a;
+
+// b = 22;
+
+// var c = b
+
+// c = 11
+
+// b = 100
+// // a = 4 < 5 && 6 > 5 && 7 > 4
+
+// console.log("a-nin deyeri", a);
+// console.log("b-nin deyeri", b);
+// console.log("c-nin deyeri", c);
+
+// var a = 10;
+// var b = 12;
+// var c = 14;
+// var d = 16;
+
+// var arrA = [10, 12, 14, 16, 25, 102, 250, 106]; // Heap
+
+// var arrB = arrA
+
+// arrA[0] = 5
+// arrB[1] = 18
+
+// console.log("A",arrA);
+// console.log("B",arrB);
+
+// var c = numbers[numbers.length - 1];
+
+// var c = numbers.length;
+
+// console.log(c);
+
+// var arr = [1, 5, "salam", true, undefined, null, [2, 5, 6, [1, 5]], [0.2, 0.3]];
+
+// console.log(arr[6][3][1]);
+
+// var text = 5
+// console.log(text.toFixed(2));
+// var num = [4.5, 5.4]
+// var tamBolunenler = [];
+
+// if (tamBolunenler.length > 0) {
+//   console.log("doludu");
+// } else {
+//   console.log("bosdu");
+// }
+
+// var c = num.concat(arr)
+
+// var emeliyyatOlunan =  arr.pop();
+// arr.unshift(100);
+
+// console.log("test",emeliyyatOlunan);
+// let newString = arr.join(" ");
+// console.log(newString);
+
+// var elifba = "a b c d e f g h j k t l m w q";
+
+// var elifbaArray = elifba.split(" ");
+// elifbaArray.push("z");
+// elifba = elifbaArray.join(" ")
+// console.log(elifba);
+
+// var newArray = arr.filter(function (item) {
+//   if (item % 2 === 0 ) {
+//     return true;
+//   }
+// });
+
+var arr = [10, 12, 14, 16, 25, 102, 250, 106, 20];
+
+// arr.splice(3, null, "sagol","hello","world");
+
+// console.log(arr);
+// let c = arr.indexOf("sagol")
+
+// let text = "salam"
+
+// let c = text.replace(/a/g,"e")
+
+// console.log(c);
+
+// var colors = ["blue", "red", "yellow", "brown"];
+
+// var answer = prompt("Which color is the best?")
+
+// if(colors.indexOf(answer) !== -1){
+//     console.log(`I think ${answer} the best`);
+// }else{
+//     console.log("Please try again new color name");
+// }
+
+// var arr = [10, 12, 20, 14, 20, 16, 25, 102, 250, 106, 20];
+
+// arr.sort((a, z) => z - a);
+// let total = arr.reduce((sum, num) => sum + num);
+// let result = arr.includes(1000)
+// let result = arr.indexOf(20,3);
+
+// console.log(arr.reverse());
+
+
+// var userName = "              SarKHaN    "
+
+// let newString = userName.trim().toLowerCase() 
+// console.log(newString);
+
+// if(userName.trim().toLowerCase() === "sarkhan"){
+//     console.log("yes");
+// }else{
+//     console.log("No");
+// }
+
+
+var arrPersonal = []
+
+let userName = prompt("Name?")
+
+arrPersonal = userName.split(" ")
+console.log(arrPersonal);
+// arrPersonal.push(userName);
+
+// console.log(arrPersonal);
