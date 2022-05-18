7aa47a4b92a2c049ed08195d9e6969951768e85f
Sarkhan Rahimli <whelp@Whelps-MacBook-Air.local>
2022-05-18 13:32:34 +0400

    update

---
+++ Fundamentals/topic15/moment/index.js
@@ -0,0 +1,71 @@
+let randomdate = "17/02/1995"
+let randomFormat = "DD-MM-YYYY"
+
+// let momentOBJ = moment()
+
+
+//Format Momment
+// let time = moment().format(randomdate,randomFormat)
+// let time = moment("17-02-1995", "DD-MM-YYYY").fromNow()
+// let time = moment(randomdate,randomFormat) //wrong
+
+// let time = momentOBJ.format("02-02-1990","DD-MM-YYYY")
+// let time = moment("16-05-2022","DD-MM-YYYY").fromNow()
+// console.log(time);
+
+
+
+
+
+//Diffrent
+// let nowTime = moment().format(randomFormat)
+
+// console.log(nowTime);
+
+// let diffYear = moment().diff(moment().format(randomFormat),"years")
+
+// let aTime = moment().format("DD/MM/YYYY")
+
+// let diffYear = moment().diff(aTime,"years")
+
+// console.log(aTime);
+// console.log(diffYear,"diffYear");
+
+
+
+// console.log(diffYear);
+
+
+var indikiTarix = moment();
+
+
+var kecmisTarix = moment("16-05-2022", "DD-MM-YYYY");
+
+let ferq = indikiTarix.diff(kecmisTarix,"hours")
+
+console.log(ferq);
+// // a.diff(b, 'years'); // 1
+
+// let illerArasindakiFerq = indikiTarix.diff(kecmisTarix, 'months'); // 24 ay
+
+// console.log(illerArasindakiFerq);
+// console.log(c);
+
+// let myObj = {    
+//     fullName:"John",
+//     islediyiZaman:illerArasindakiFerq,
+//     umumiAldigimaaas:illerArasindakiFerq * 2000, 
+//     maas:2000
+// }
+
+// let time = moment().set('year', 2022) ;
+
+// console.log(time);
+// console.log(moment().diff(moment(randomdate, "DD-MM-YYYY"), "years"));
+
+//Now
+// console.log(moment(randomdate,randomFormat).fromNow());
+// console.log(moment(randomdate,randomFormat).toNow());
+
+
+
